241dc575f29bfbf21a365f0863cb57425b9fa52d
Gayathri Ganesh <47293060+gayathrig269@users.noreply.github.com>
2021-06-26 23:24:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import yfinance as yf # https://pypi.org/project/yfinance/
 import ta
 from ta.volatility import BollingerBands
